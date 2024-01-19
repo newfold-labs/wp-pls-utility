eefe6954e94b624e814bf80b1572e90824e7f6fc
Francesco Licandro <francesco.licandro@yithemes.com>
2024-01-19 10:08:01 +0100

    check if REST API class exist before use

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,9 @@ defined( 'ABSPATH' ) || exit;
 add_action(
 	'wp_loaded',
 	function () {
+		if ( ! class_exists( 'NewfoldLabs\WP\PLS\Rest\v1\Rest' ) ) {
+			return;
+		}
 		// initialize REST.
 		new NewfoldLabs\WP\PLS\Rest\v1\Rest();
 	}
